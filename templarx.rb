166182c60d9727895f89b9beecf7e07608c4c460
Devin Chalmers <devin.chalmers@gmail.com>
2009-02-04 01:12:30 -0800

    Few tweaks for Compypatibility

--- templarx.rb
+++ templarx.rb
@@ -1,24 +1,25 @@
 require 'erb'
+require 'fileutils'
 
 class Templarx  
-  TEMPLATE_PATH = File.join(File.dirname(__FILE__), '/drum_definition.erb')
+  TEMPLATE_PATH = File.join(File.dirname(__FILE__), 'drum_definition.erb')
 
   attr_accessor :probabilities
-  attr_accessor :default_probability
-  attr_accessor :start_channel
+  attr_accessor :default
   
   def initialize opts={}
-    opts = {:probability => 0.5,
-            :definition_path => File.join(File.dirname(__FILE__), 'test.rb'),
-            :start_channel => 36}.merge opts
+    opts = {:default => 0.5,
+            :definition_path => File.join(File.dirname(__FILE__), 'test.rb')}.merge opts
     @ddpath = opts[:definition_path]
-    @default_probability = opts[:probability] 
+    @default = opts[:default]
+    @probabilities = opts[:probabilities]
   end
 
   # 10x16 array of 0.0-1.0
   # rows represent midi channels, columns sixteenth notes
   def initialize_probabilities
-    @probabilities = (0..9).map{Array.new(16, @default_probability)}
+    @probabilities = {}
+    (36..45).each{|i| @probabilities[i] = Array.new(16, @default)}
   end
   
   def template
@@ -26,7 +27,6 @@ class Templarx
   end
   
   def rewrite_drum_definition
-    initialize_probabilities
     e = ERB.new template
     File.open(@ddpath, "w"){|f| f << e.result(binding)}
   end
